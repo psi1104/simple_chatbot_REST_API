01ff204869a816ee969d6dc32f738423c6580364
= <=>
2020-05-24 16:10:19 +0900

    create simple_chatbot.py

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
 from flask import Flask, request, jsonify
+from simple_chatbot import chatbot
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    message = '''
-              Hello, world! 
-              This is simple-chatbot! 
-              Please 
-              '''
+    message = 'Hello, world! This is simple-chatbot!'
+    return message
+
+@app.route('/post', methods=['POST'])
+def hello_post():
+    message = 'Hello, POST! This is simple-chatbot!'
+    return message
+
+@app.route('/put', methods=['PUT'])
+def hello_put():
+    message = 'Hello, PUT! This is simple-chatbot!'
+    return message
+
+@app.route('/delete', methods=['DELETE'])
+def hello_delete():
+    message = 'Hello, DELETE! This is simple-chatbot!'
     return message
 
 @app.route('/chatbot')
-def chatbot():
+def reply():
     sentence = request.args.get('sentence')
-    reply = ""
 
-    if sentence == "Hello":
-        reply = "Hi!"
-
-    return jsonify({
-                    "sentence":sentence,
-                    "reply":reply
-                    })
+    return jsonify(chatbot(sentence))
  
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port='5000')
+    app.run(debug=True, port='5000')
---
+++ simple_chatbot.py
@@ -0,0 +1,20 @@
+def chatbot(sentence):
+    reply = ""
+
+    if sentence == "Hello":
+        reply = "Hi!"
+
+    elif sentence == "What is your name?":
+        reply = "My name is chatbot!"
+
+    elif sentence == "Where are you located":
+        reply = "I located seoul!"
+
+    else:
+        reply = "Sorry, I don't understand..."
+    sentence_reply = {
+                        "sentence":sentence,
+                        "reply":reply
+                    }
+                    
+    return sentence_reply
